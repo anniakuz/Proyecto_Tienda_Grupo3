dec2b7befa48f7c9a1bef77d97818f905a6dabcf
AnniaKuzmina <107867033+anniakuz@users.noreply.github.com>
2024-05-31 11:47:49 +0200

    Update ClientePorNumeroDeCompras.java

--- comandos/ClientePorNumeroDeCompras.java
+++ comandos/ClientePorNumeroDeCompras.java
@@ -15,43 +15,26 @@ public class ClientePorNumeroDeCompras implements Comando{
     public void ejecutar() {
         reloadProductos();
         reloadCompras();
-        Map<String, Double> gananciasPorCliente = new HashMap<>();
-        //recorremos la lista de las compras
-        for(int i = 0; i < compras.size();i++){
-                //recorremos la lista de productos
-            for (int j = 0; j < productos.size(); j++) {
-                if (productos.get(j).getNombre().equalsIgnoreCase(compras.get(i).getNombreProducto())) {
-                    //si encontramos el producto cogemos su precio en Producto y su cantidad en Compra y calculamos la totalidad de esta compra
-                    double total = 0.0;
-                    total += compras.get(i).getCantidad() * productos.get(j).getPrecio();
+        //Contamos la cantidad de veces que un cliente ha compra y lo guardamos en un map con el nombre de cliente como key
+        // y la cantidad de compras como el Value
+        Map<String, Long> cantidadCompras = compras.stream()
+                .collect(Collectors.groupingBy(
+                        Compra::getNombreClinete,
+                        counting()
+                ));
 
-                    if(!gananciasPorCliente.containsKey(compras.get(i).getNombreClinete())){
-                        //si no encontramos al cliente en el map, significa que es su primera compra, entonces lo guardamos tal cual
-                        gananciasPorCliente.put(compras.get(i).getNombreClinete(), total);
-                    }else if(gananciasPorCliente.containsKey(compras.get(i).getNombreClinete())){
-                        double nuevaTotal = 0.0;
-                        //si encontramos al cliente, actualizamos la totalidad 
-                        nuevaTotal = total +gananciasPorCliente.get(compras.get(i).getNombreClinete());
-                        gananciasPorCliente.put(compras.get(i).getNombreClinete(), nuevaTotal);
-                    }
-                }
+        //en el map buscamos el value maximo que representa la cantidad de compras
+        Long numeroCompras = Collections.max(cantidadCompras.entrySet(),
+                (entry1, entry2) -> Math.toIntExact(entry1.getValue() - entry2.getValue())).getValue();
+
+        //tenemos en cuenta que mas de un cliente puede hacer la misma cantidad de compras,
+        //por eso hacemos el for para recorrer el map y sacar los nombres de los clientes con el value maximo de compra
+        System.out.println("Clientes mas activos tienen " + numeroCompras + " compras");
+        for (Map.Entry<String, Long> entry : cantidadCompras.entrySet()) {
+            if (entry.getValue() == numeroCompras) {
+                System.out.println(entry.getKey());
             }
         }
-        //encontamos el value mas grande en el map
-
-        Double totalMax = Collections.max(gananciasPorCliente.entrySet(),
-                (entry1, entry2) -> Math.toIntExact((long) (entry1.getValue() - entry2.getValue()))).getValue();
-
-        
-        //tenemos en cuenta que pueden haber más de un cliente con la misma totalidad de las compras
-        //por eso, recorremos el map para comprobar si alguien más tiene la misma totalidad e imprimimos 
-        System.out.println("Clientes mas activos tienen " + totalMax + "  euros total en compras");
-        for (Map.Entry<String, Double> entry : gananciasPorCliente.entrySet()) {
-            if (Objects.equals(entry.getValue(), totalMax)) {
-                System.out.println(entry.getKey()+ "    "+ entry.getValue());
-            }
-        }
-
     }
     public void reloadProductos(){
         productos = ProductoData.leerTodosProductos();
