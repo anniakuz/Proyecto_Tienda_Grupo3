0d137426bd58c1ed2f04de5d22dab31568d3214b
AngelCorral0 <angel.corral.olivares@gmail.com>
2024-05-31 10:31:12 +0200

    added a coment

--- comandos/VerCompras.java
+++ comandos/VerCompras.java
@@ -15,6 +15,8 @@ public class VerCompras implements Comando {
         System.out.println("Lista de comandas.csv:" + "\n");
         compras.stream().forEach(producto -> System.out.println(producto.toString()));
         /*un stream en Java permite realizar operaciones sobre colecciones de manera funcional y declarativa, facilitando la manipulación de datos de una forma más concisa y legible.*/
+
+        /*Una expresión lambda en Java es una forma concisa de representar una instancia de una interfaz funcional */
     }
 
     public void reloadCompras() {
