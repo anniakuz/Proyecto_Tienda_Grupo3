16ab9dbaea2e4c1dacb2c9826d31ef3d969e2d59
jiajun10901232 <115166807+jiajun10901232@users.noreply.github.com>
2024-05-31 10:00:21 +0200

    comentario en mi parte del codigo

--- comandos/VerProductos.java
+++ comandos/VerProductos.java
@@ -1,23 +1,25 @@
 package comandos;
 
-
-
 import entidades.Producto;
 import lectorArchivos.ProductoData;
 
 import java.util.List;
 
-public class VerProductos implements Comando{
-    List<Producto> productos;
+public class VerProductos implements Comando {
+    List<Producto> productos;  // Lista para almacenar todos los productos leídos
 
-    public void ejecutar(){
+    // Método que se ejecuta para mostrar la lista de productos
+    public void ejecutar() {
+        // Recargar la lista de productos desde el archivo
         reloadProductos();
+        // Mostrar un mensaje indicando que se va a listar los productos
         System.out.println("Lista de productos.csv:");
+        // Recorrer la lista de productos y mostrarlos en la consola
         productos.stream().forEach(producto -> System.out.println(producto.toString()));
     }
 
-    public void reloadProductos(){
+    // Método para recargar la lista de productos desde el archivo
+    public void reloadProductos() {
         productos = ProductoData.leerTodosProductos();
     }
-
 }
