787464627161885bfdb1d03a15d30e3a8247523f
AngelCorral0 <angel.corral.olivares@gmail.com>
2024-05-31 10:06:25 +0200

    Comentarios en comando y cambio de comanda

--- comandos/CambioDeComanda.java
+++ comandos/CambioDeComanda.java
@@ -8,6 +8,11 @@ public class CambioDeComanda {
 
     {
         comandoMap = new HashMap<>();
+        /*Map es un tipo de colección con la estructura de clave/valor
+        guardamos los comandos con el string de un numero y Comando*/
+
+        /*command pattern hace que cada petición sea una clase que contiene toda la información y la implementación necesaria
+        permite implementar el sistema de comandos de manera más fácil y organizada*/
     }
 
     public void registrarComanda(String comandoNombre, Comando comando) {
--- comandos/Comando.java
+++ comandos/Comando.java
@@ -2,4 +2,7 @@ package comandos;
 
 public interface Comando {
     void ejecutar();
+    /*Interfaz es un componente de la poo y se trata de una plantilla que permite preesteblecer el contenido y/o
+    comportamiento de una o varias clases. Se establece un conjunto de reglas que las clases que impementen la interfaz
+    deberan seguir. En nuestro caso, todas las clases tendran que tener el método ejecutar();*/
 }
